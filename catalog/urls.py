0df18ba2003d9be4baf813906bf99b4fe89d3504
Zhenya Kudrya <megusta997@gmail.com>
2024-01-16 21:10:19 +0300

    hw 5

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import CategoryListView, contacts, product
+from catalog.views import CategoryListView, contacts, ProductDetailView
 from catalog.views import ProductsListView, CategoryProductsListView
 
 app_name = CatalogConfig.name
@@ -11,5 +11,5 @@ urlpatterns = [
     path('products/', ProductsListView.as_view(), name='products'),
     path('contacts/', contacts, name='contacts'),
     path('<int:pk>/catalog/', CategoryProductsListView.as_view(), name='category_products'),
-    path('<int:id>/product/', product, name='product'),
+    path('<int:pk>/product/', ProductDetailView.as_view(), name='product'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -39,34 +39,35 @@ class CategoryProductsListView(ListView):
         return context_data
 
 
-def product(requests, id):
-    product = Product.objects.get(id=id)
-    context = {
-        'product_name': product.product_name,
-        'product_content': product.product_content,
-        'title': 'Выбранный товар',
-        'product_image': product.product_image,
-        'price_for_one': f'Цена: {product.price_for_one} руб.'
-    }
-    return render(requests, 'catalog/product.html', context)
+# def product(requests, id):
+#     product = Product.objects.get(id=id)
+#     context = {
+#         'product_name': product.product_name,
+#         'product_content': product.product_content,
+#         'title': 'Выбранный товар',
+#         'product_image': product.product_image,
+#         'price_for_one': f'Цена: {product.price_for_one} руб.'
+#     }
+#     return render(requests, 'catalog/product.html', context)
 
 
-# class ProductDetailView(DetailView):
-#     model = Product
-#
-#     def get_queryset(self):
-#         queryset = super().get_queryset()
-#         # queryset = queryset.filter(id=self.kwargs.get('id'))
-#         return queryset
-#
-#     def get_context_data(self, *args, **kwargs):
-#         context_data = super().get_context_data(*args, **kwargs)
-#
-#         product = Product.objects.get(id=self.kwargs.get('id'))
-#         context_data['product_name'] = product.product_name
-#         context_data['product_content'] = product.product_content
-#         context_data['title'] = 'Выбранный товар'
-#         context_data['product_image'] = product.product_image
-#         context_data['price_for_one'] = f'Цена: {product.price_for_one} руб.'
-#
-#         return context_data
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'catalog/product.html'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(id=self.kwargs.get('pk'))
+        return queryset
+
+    def get_context_data(self, *args, **kwargs):
+        context_data = super().get_context_data(*args, **kwargs)
+
+        product = Product.objects.get(id=self.kwargs.get('pk'))
+        context_data['product_name'] = product.product_name
+        context_data['product_content'] = product.product_content
+        context_data['title'] = 'Выбранный товар'
+        context_data['product_image'] = product.product_image
+        context_data['price_for_one'] = f'Цена: {product.price_for_one} руб.'
+
+        return context_data
